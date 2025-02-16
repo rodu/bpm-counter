2442ca66381219abaa0191f8afb156ce4fff638b
rodu <3225111+rodu@users.noreply.github.com>
2025-02-16 19:35:07 +0000

    Enables PWA support via Vite plugin PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'BPM Counter',
+        short_name: 'Count BPMs by tapping',
+        description:
+          'An app to find the BPM of a track by tapping keyboard keys',
+        theme_color: '#ffffff',
+        /* icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ], */
+      },
+    }),
+  ],
+});
