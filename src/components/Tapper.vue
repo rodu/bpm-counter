c32fe4bf4c398a70e84794a4a9cb98706c4d9e4e
rodu <3225111+rodu@users.noreply.github.com>
2025-02-17 20:07:29 +0000

    Adds pulse animation to represent the BPM value visually

--- src/components/Tapper.vue
+++ src/components/Tapper.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { fromEvent, map, pairwise, scan, timestamp } from 'rxjs';
+import { ref, useTemplateRef } from 'vue';
+import { fromEvent, map, pairwise, scan, tap, timestamp } from 'rxjs';
 
 const bpm = ref(0);
 const hits = ref(0);
@@ -10,10 +10,16 @@ const resetTimeout = 2000;
 
 // Create an Observable that emits events of a specific type, here 'keydown'
 const keypress$ = fromEvent<KeyboardEvent>(target, 'keyup');
-
+/* const tapperClick$ = fromEvent<MouseEvent>(
+  document.querySelector('#tapper')!,
+  'click'
+);
+ */
 // Subscribe to the observable to handle key press events
 keypress$
   .pipe(
+    tap((event: Event) => pulseOnce(event)),
+
     // Step 1: Add timestamps to each emission
     timestamp(),
 
@@ -55,11 +61,65 @@ keypress$
     bpm.value = bpmValue;
     hits.value = hitsValue;
   });
+
+const shouldPulsate = ref(false);
+const tapperDiv = useTemplateRef('tapper');
+const pulseOnce = (event: Event) => {
+  event.stopPropagation();
+
+  if (hits.value > 20) {
+    shouldPulsate.value = true;
+
+    // Sets the pulse animation to the same speet of the calculated BPM
+    if (tapperDiv.value) {
+      tapperDiv.value.style.animationDuration =
+        (60 / bpm.value).toFixed(3) + 's';
+    }
+  } else {
+    shouldPulsate.value = false;
+  }
+};
 </script>
 
 <template>
-  <div>BPM: {{ bpm }}</div>
+  <div ref="tapper" class="bpm-container" :class="{ pulse: shouldPulsate }">
+    <div>BPM</div>
+    <div>{{ bpm }}</div>
+  </div>
   <div>Hits: {{ hits }}</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.bpm-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  background-color: #f8a978;
+  color: #222;
+  font-size: 1.5rem;
+  border-radius: 50%;
+  user-select: none;
+  box-shadow: 0px 25px 50px -12px rgba(255, 255, 255, 0.25);
+}
+
+.pulse {
+  animation: 0.25s pulse infinite;
+}
+
+@keyframes pulse {
+  0% {
+    box-shadow: 0 0 0 0 rgba(255, 255, 255, 0.7);
+  }
+
+  70% {
+    box-shadow: 0 0 0 15px rgba(255, 255, 255, 0);
+  }
+
+  100% {
+    box-shadow: 0 0 0 0 rgba(255, 255, 255, 0);
+  }
+}
+</style>
